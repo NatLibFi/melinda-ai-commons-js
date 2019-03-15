15d6e7f3b58becae3ec49f88236fbeb4ce7dd4be
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-03-15 08:56:57 +0200

    Remove obsolete debug import

--- src/similarity/utils.js
+++ src/similarity/utils.js
@@ -26,12 +26,9 @@
  *
  **/
 
-import createDebugLogger from 'debug';
 import _ from 'lodash';
 import {MarcRecord} from '@natlibfi/marc-record';
 import * as Extractors from './feature-extractors';
-
-const debug = createDebugLogger('@natlibfi/melinda-ai-commons/similarity');
 
 export function toxmljsFormat(marcRecord) {
 	const xmljsFormat = {
